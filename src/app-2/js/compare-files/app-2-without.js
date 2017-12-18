c771514381efd1fe89ff9d92f7af3021f1e183b0
Ankit Patel <Ankit.Patel@icrossing.com>
2017-12-18 10:12:37 +0530

    Scope hoisting

---
+++ src/app-2/js/compare-files/app-2-without.js
@@ -0,0 +1,128 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__import_1__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__import_2__ = __webpack_require__(2);
+
+
+
+//import css from '../css/app-2.scss';
+console.log("this is app2 js");
+
+class MyClass {
+    constructor() {
+        this._initImports();
+    }
+
+    _initImports() {
+        let import1 = __WEBPACK_IMPORTED_MODULE_0__import_1__["a" /* default */].default,
+            import2 = __WEBPACK_IMPORTED_MODULE_1__import_2__["a" /* default */].default;
+
+            new import1();
+            new import2();
+    }
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/**
+ * class Import1
+ */
+class Import1 {
+    constructor() {
+        console.log('This is import 1 file');
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Import1;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/**
+ * class Import2
+ */
+class Import2 {
+    constructor() {
+        console.log('This is import 2 file');
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Import2;
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=app-2.js.map
---
+++ src/app-2/js/import-2.js
@@ -0,0 +1,8 @@
+/**
+ * class Import2
+ */
+export default class Import2 {
+    constructor() {
+        console.log('This is import 2 file');
+    }
+}
